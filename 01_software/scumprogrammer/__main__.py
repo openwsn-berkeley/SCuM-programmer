53ea104aec876a4d51ee2f467cf9c1f821399861
Thomas Watteyne <twatteyne@gmail.com>
2021-09-12 02:49:15 +0200

    #15. moving main around

---
+++ 01_software/scumprogrammer/__main__.py
@@ -0,0 +1,14 @@
+# built-in
+# third party
+# local
+
+#========================== classes =========================================
+
+#========================== main ==============================================
+
+def main():
+    print('This is the SCuM program')
+    #fancyCli = FancyCli.FancyCli()
+
+if __name__== "__main__":
+    main()
