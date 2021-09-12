0024d9c2e3c84746534713949f271343f83dc2c9
Thomas Watteyne <twatteyne@gmail.com>
2021-09-12 03:35:47 +0200

    #15. Basic working CLI.

--- 01_software/scumprogrammer/InterfaceCli.py
+++ 01_software/scumprogrammer/InterfaceCli.py
@@ -2,7 +2,7 @@
 import threading
 import time
 # third party
-import blessed
+# local
 
 class InterfaceCli(threading.Thread):
     BANNER = [
@@ -15,7 +15,6 @@ class InterfaceCli(threading.Thread):
     '         www.crystalfree.org',
     ]
     def __init__(self, *a, **kw):
-        self.term = blessed.Terminal()
         super(InterfaceCli, self).__init__(*a, **kw)
         self.name = 'InterfaceCli'
         self.start()
@@ -23,34 +22,14 @@ class InterfaceCli(threading.Thread):
     def run(self):
         try:
             self._playIntro()
-            self._createPanels()
-            curserOn = True
-            with self.term.cbreak():
-                val = ''
-                while val.lower() != 'q':
-                    val = self.term.inkey(timeout=0.5)
-                    if val:
-                        print(self.term.gold_on_blue(val)+self.term.move_up(1))
-                        print(self.term.move_right(2))
-                    else:
-                        curserOn = not curserOn
-                        with self.term.location():
-                            if curserOn:
-                                print(self.term.on_gold(' '))
-                            else:
-                                print(self.term.on_blue(' '))
-        except:
-            print('CRITICAL!!!')
+            while True:
+                cmd = input()
+                cmd = cmd.strip()
+                if cmd=='q':
+                    break
+        except Exception as err:
+            print(type(err))
+            print(err)
     
     def _playIntro(self):
-        with self.term.hidden_cursor():
-            print(self.term.home + self.term.gold_on_blue + self.term.clear + self.term.move_y((self.term.height // 2)-(len(self.BANNER)//2+1)))
-            for line in self.BANNER:
-                print(self.term.center(line))
-                time.sleep(0.15)
-                #print(self.term.move_down(1))
-            time.sleep(0.5)
-            print(self.term.home + self.term.clear)
-    
-    def _createPanels(self):
-        pass
+        print('\n'.join(self.BANNER))
--- 01_software/scumprogrammer/ScumConnector.py
+++ 01_software/scumprogrammer/ScumConnector.py
@@ -1,4 +1,41 @@
+# built-in
+import threading
+import time
+# third party
+import serial
+# local
 
-
-class ScumConnector(object):
-    pass
+class ScumConnector(threading.Thread):
+    
+    def __init__(self,serialport=None):
+        
+        # store params
+        self.serialport      = serialport
+        
+        # local variables
+        self.datalock        = threading.RLock()
+        self.isconnected     = False
+        
+        # initialize thread
+        super(ScumConnector, self).__init__()
+        self.name            = 'ScumConnector'
+        self.daemon          = True
+        self.start()
+    
+    def run(self):
+    
+        while True:
+            try:
+                time.sleep(1)
+                ser = serial.Serial(self.serialport,19200)
+            except Exception as err:
+                print(type(err))
+                print(err)
+    
+    #======================== public ==========================================
+    
+    def set_serialport(self,serialport):
+        with self.datalock:
+            self.serialport = serialport
+    
+    #======================== private =========================================
