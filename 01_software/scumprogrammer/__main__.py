8b1079582d4211b78e53bc59995ca7e7ed6f1b95
Thomas Watteyne <twatteyne@gmail.com>
2021-09-12 02:54:15 +0200

    #15. cleaning up VERSION and main()

--- 01_software/scumprogrammer/__main__.py
+++ 01_software/scumprogrammer/__main__.py
@@ -1,13 +1,14 @@
 # built-in
 # third party
 # local
+import VERSION
 
 #========================== classes =========================================
 
 #========================== main ==============================================
 
 def main():
-    print('This is the SCuM program')
+    print('This is the SCuM {} running'.format(VERSION.VERSION))
     #fancyCli = FancyCli.FancyCli()
 
 if __name__== "__main__":
