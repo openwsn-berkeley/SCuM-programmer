c8a40eb7af2d918b3d9fc120920a14c7155f32ff
Thomas Watteyne <twatteyne@gmail.com>
2021-09-12 02:46:30 +0200

    #15. breaking out FancyCli

--- 01_software/scumprogrammer/scumprogrammer.py
+++ 01_software/scumprogrammer/scumprogrammer.py
@@ -1,78 +1,21 @@
 # built-in
-import random
-import threading
-import time
 # third party
-import serial
-import blessed
 # local
-import OpenHdlc
+import FancyCli
 
 #========================== classes =========================================
-
-#=== FancyCli
-
-class FancyCli(threading.Thread):
-    BANNER = [
-    '   _____  _____      __  __ ',
-    '  / ____|/ ____|    |  \/  |',
-    ' | (___ | |    _   _| \  / |',
-    '  \___ \| |   | | | | |\/| |',
-    '  ____) | |___| |_| | |  | |',
-    ' |_____/ \_____\__,_|_|  |_|',
-    '         www.crystalfree.org',
-    ]
-    def __init__(self, *a, **kw):
-        self.term = blessed.Terminal()
-        super(FancyCli, self).__init__(*a, **kw)
-        self.name = 'FancyCli'
-        self.start()
-    
-    def run(self):
-        try:
-            self._playIntro()
-            self._createPanels()
-            curserOn = True
-            with self.term.cbreak():
-                val = ''
-                while val.lower() != 'q':
-                    val = self.term.inkey(timeout=0.5)
-                    if val:
-                        print(self.term.gold_on_blue(val)+self.term.move_up(1))
-                        print(self.term.move_right(2))
-                    else:
-                        curserOn = not curserOn
-                        with self.term.location():
-                            if curserOn:
-                                print(self.term.on_gold(' '))
-                            else:
-                                print(self.term.on_blue(' '))
-        except:
-            print('CRITICAL!!!')
-    
-    def _playIntro(self):
-        with self.term.hidden_cursor():
-            print(self.term.home + self.term.gold_on_blue + self.term.clear + self.term.move_y((self.term.height // 2)-(len(self.BANNER)//2+1)))
-            for line in self.BANNER:
-                print(self.term.center(line))
-                time.sleep(0.15)
-                #print(self.term.move_down(1))
-            time.sleep(0.5)
-            print(self.term.home + self.term.clear)
-    
-    def _createPanels(self):
-        pass
 
 #=== app
 
 class ScumProgrammer(object):
     def __init__(self):
-        fancyCli = FancyCli()
+        fancyCli = FancyCli.FancyCli()
 
 #========================== main ==============================================
 
 def main():
     print('This is the SCuM program')
+    scumProgrammer = ScumProgrammer()
 
 if __name__=='__main__':
     main()
